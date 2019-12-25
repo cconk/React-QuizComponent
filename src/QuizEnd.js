b786593c88cd0465dd5dbe5d5b1b51e775aaa24f
chadc <chadconklin@outlook.com>
2019-12-25 15:02:54 -0800

    Completed module

---
+++ src/QuizEnd.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import QuizQuestionButton from './QuizQuestionButton'
+
+class QuizEnd extends Component {
+    render () {
+        return (
+            <div>
+                <p>Thanks for playing!</p>
+
+                <a href=''>Reset Quiz</a>
+            </div>
+        )
+    }
+}
+
+export default QuizEnd
